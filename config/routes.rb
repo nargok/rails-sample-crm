f6ad475046654c223d86cef8b8193e1a12cdb945
nargok <nargok@gmail.com>
2017-12-31 08:19:55 +0000

    Put hostname in staff & admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,29 @@
 Rails.application.routes.draw do
- namespace :staff, path: '' do
-  root 'top#index'
-  get 'login' => 'sessions#new', as: :login
-  resource :session, only: [ :create, :destroy ]
-  resource :account, except: [ :new, :create, :destroy ]
- end
+  config = Rails.application.config.baukis
 
- namespace :admin do
-  root 'top#index'
-  get 'login' => 'sessions#new', as: :login
-  resource :session, only: [ :create, :destroy ]
-  resources :staff_members
- end
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :account, except: [ :new, :create, :destroy ]
+    end
+  end
 
- namespace :customer do
-  root 'top#index'
- end
+  constraints host: config[:staff][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: [ :create, :destroy ]
+      resources :staff_members
+    end
+  end
 
- root 'errors#not_found'
- get '*anything' => 'errors#not_found'
+  namespace :customer do
+   root 'top#index'
+  end
+
+  root 'errors#not_found'
+  get '*anything' => 'errors#not_found'
 
 end
