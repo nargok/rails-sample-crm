ef91bca510f359eab78bd9950a4f0fe6dc438859
nargok <nargok@gmail.com>
2018-01-12 07:07:42 +0000

    Performance maintenance for staff_events log

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -5,12 +5,13 @@ class Admin::StaffEventsController < Admin::Base
       @staff_member = StaffMember.find(params[:staff_member_id])
       # ActiveRecord::Relationクラスのインスタンスを取得する
       # この時点ではまだクエリは発行されない。クエリのための検索条件を指定しているだけ
-      @events = @staff_member.events.order(occured_at: :desc)
+      @events = @staff_member.events
     else
       # 全職員のイベントを見る
-      @events = StaffEvent.order(occured_at: :desc)
+      @events = StaffEvent
     end
     # クエリ発行のための検索条件を追加している
-    @events = @events.page(params[:page])
+    @evetns = @events.order(occured_at: :desc)
+      .includes(:member).page(params[:page])
   end
 end
