13daa20b9775744d614ad82e8478f97bf194d418
nargok <nargok@gmail.com>
2018-01-12 06:41:33 +0000

    pagination

--- Gemfile
+++ Gemfile
@@ -42,7 +42,8 @@ gem 'therubyracer', platforms: :ruby
 gem 'bcrypt', '~> 3.1.7'
 gem 'nokogiri', '~> 1.6.1'
 gem 'rails-i18n', '~> 4.0.1'
-gem 'foreigner', '~> 1.6.1'
+gem 'foreigner', '~> 1.6.1'  # 外部キーを指定するGem
+gem 'kaminari', '~> 0.15.1'
 
 group :test do
 	gem 'rspec-rails', '~> 3.0.0.beta2'
--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -3,10 +3,14 @@ class Admin::StaffEventsController < Admin::Base
     if params[:staff_member_id]
       # 特定の職員のイベントを見る
       @staff_member = StaffMember.find(params[:staff_member_id])
+      # ActiveRecord::Relationクラスのインスタンスを取得する
+      # この時点ではまだクエリは発行されない。クエリのための検索条件を指定しているだけ
       @events = @staff_member.events.order(occured_at: :desc)
     else
       # 全職員のイベントを見る
       @events = StaffEvent.order(occured_at: :desc)
     end
+    # クエリ発行のための検索条件を追加している
+    @events = @events.page(params[:page])
   end
 end
