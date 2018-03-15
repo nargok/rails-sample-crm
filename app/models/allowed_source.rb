0253a9ada55a8c677b8bdffcfa54974365832063
Ryoichi Gokan <nargok@gmail.com>
2018-03-15 23:33:40 +0900

    IPアドレス制限機能の実装：allowed_sourceモデル作成

---
+++ app/models/allowed_source.rb
@@ -0,0 +1,22 @@
+class AllowedSource < ActiveRecord::Base
+  validates :octet1, :octet2, :octet3, :octet4, presence: true,
+            numericality: { only_integer: true, allow_bank: true },
+            inclusion: { in: 0..255, allow_blank: true }
+  # octed1 ~ octet4までの組み合わせが一意であること
+  validates :octet4,
+            uniqueness: { scope: [ :octet1, :octet2, :octet3 ], allow_blank: true }
+
+  # ipアドレスを文字列でも指定できるようにする
+  def ip_address=(ip_address)
+    octets = ip_address.split('.')
+    self.octet1 = octets[0]
+    self.octet2 = octets[1]
+    self.octet3 = octets[2]
+    if octets[3] == '*'
+      self.octet4 = 0
+      self.wildcard = true
+    else
+      self.octet4 = octets[3]
+    end
+  end
+end
