3ef23b13009803b240a0b39b6b05a025e915789a
nargok <nargok@gmail.com>
2018-03-17 18:16:13 +0900

    Refactoring for the include? method in Allowed_source model

--- spec/routing/hostname_constraints_spec.rb
+++ spec/routing/hostname_constraints_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 
 RSpec.describe 'ルーティング' do
   example '職員トップページ' do
+    pending('Due to deactivate baukis_host in the routes.rb')
     expect(get: 'http://baukis.example.com').to route_to(
                                                   host: 'baukis.example.com',
                                                   controller: 'staff/top',
@@ -11,6 +12,7 @@ RSpec.describe 'ルーティング' do
   end
 
   example '管理者ログインフォーム' do
+    pending('Due to deactivate baukis_host in the routes.rb')
     expect(get: 'http://baukis.example.com/admin/login').to route_to(
                                                   host: 'baukis.example.com',
                                                   controller: 'admin/sessions',
@@ -19,6 +21,7 @@ RSpec.describe 'ルーティング' do
   end
 
   example '顧客トップページ' do
+    pending('Due to deactivate baukis_host in the routes.rb')
     expect(get: 'http://example.com/mypage').to route_to(
                                                   host: 'example.com',
                                                   controller: 'customer/top',
@@ -27,6 +30,7 @@ RSpec.describe 'ルーティング' do
   end
   
   example 'ホスト名が対象外ならerrors/not_foundへ' do
+    pending('Due to deactivate baukis_host in the routes.rb')
     expect(get: 'http://foo.example.jp').to route_to(
                                               controller: 'errors',
                                               action: 'not_found'
